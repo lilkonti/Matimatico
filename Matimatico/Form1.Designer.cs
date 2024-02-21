d8fac83ac069c0f7d1ee757a6d55371b600cee62
Student <Student@DESKTOP>
2024-02-21 19:41:51 +0200

    f

--- Matimatico/Form1.Designer.cs
+++ Matimatico/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelU = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelU
+            // 
+            this.labelU.AutoSize = true;
+            this.labelU.Location = new System.Drawing.Point(12, 188);
+            this.labelU.Name = "labelU";
+            this.labelU.Size = new System.Drawing.Size(32, 13);
+            this.labelU.TabIndex = 0;
+            this.labelU.Text = "User:";
+            this.labelU.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelU);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelU;
     }
 }
 
